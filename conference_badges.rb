e20012d1bae5b5bdcfa0d80a80942a732c2421fa
olga gomelskiy <olichka23.85@gmail.com>
2021-04-13 03:38:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+
+def batch_badge_creator()
+end
+
+def assign_rooms()
+end
+
+def printer()
+end
